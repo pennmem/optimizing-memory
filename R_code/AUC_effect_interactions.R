88f94e95170c31b15cea3194d4c505ca43f4547d
Joseph Rudoler <jrudoler56@gmail.com>
2023-03-27 12:23:05 -0400

    output three way model results

--- R_code/AUC_effect_interactions.R
+++ R_code/AUC_effect_interactions.R
@@ -2,19 +2,23 @@ library(lme4)
 library(lmerTest)
 library(emmeans)
 library(tidyverse)
+library(broom.mixed)
 
-df <- read.csv("Documents/GitHub/optimizing-memory/data/processed_events_with_AUC.csv")
+df <- read.csv("~/optimizing-memory/data/processed_events_with_AUC.csv")
 data <- df |>
   filter(type == "WORD", trial_type != "NoStim")
 
 sub_recalled_score_df <- data %>% group_by(subject, trial_type, AUC) |>
   summarize(sub_recalled = mean(recalled)) |>
   mutate(
-    trial_type = factor(trial_type, levels = c("Sham", "Neg", "Pos")),
+    trial_type = factor(trial_type, levels = c("Neg", "Sham", "Pos")),
     AUC_cent = AUC - .5
   )
 three_way_model <- lmer(sub_recalled ~ trial_type * AUC_cent + (1 | subject), data=sub_recalled_score_df)
 summary(three_way_model)
+
+broom_three_way_model <- tidy(three_way_model)
+write.csv(broom_three_way_model, file = "~/optimizing-memory/results/three_way_model_neg_intercept.csv")
 
 #ANOVA for interaction terms
 anova(three_way_model)
@@ -34,3 +38,4 @@ trial_type.emm <- emmeans(RG.max_AUC, pairwise ~ trial_type)
 summary(trial_type.emm, adjust = "none")
 #tukey adjustment (probably correct)
 summary(trial_type.emm)
+
