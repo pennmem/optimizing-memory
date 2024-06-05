a2cdbb2ba7b27966372c93bbf2b568a34dbc4cef
Joseph Rudoler <jrudoler56@gmail.com>
2024-06-05 16:47:01 -0400

    update R code with comments

--- R_code/AUC_effect_interactions.R
+++ R_code/AUC_effect_interactions.R
@@ -4,21 +4,27 @@ library(emmeans)
 library(tidyverse)
 library(broom.mixed)
 
+# read in properly labeled data
 events <- read.csv("~/optimizing-memory/data/processed_events_with_AUC.csv")
+
+# select only WORD trials with one of the three "stim" trial types
 data <- events |>
   filter(type == "WORD", trial_type != "NoStim")
 
+# group by subject and trial type and calculate the average recall
 sub_recalled_score_df <- data |> group_by(subject, trial_type, AUC) |>
   summarize(sub_recalled = mean(recalled)) |>
   mutate(
+    # code trial type as factor for negative, sham, positive
     trial_type = factor(trial_type, levels = c("Neg", "Sham", "Pos")),
-    AUC_cent = AUC - .5
+    AUC_cent = AUC - .5 # center AUC at .5 (chance level prediction) for easier interpretation
   )
+# model with fixed effects of trial type and AUC and interaction, also random intercepts for subject
 three_way_model <- lmer(sub_recalled ~ trial_type * AUC_cent + (1 | subject), data=sub_recalled_score_df)
 summary(three_way_model)
 
 broom_three_way_model <- tidy(three_way_model, conf.int=TRUE)
-write.csv(broom_three_way_model, file = "~/optimizing-memory/results/three_way_model_neg_intercept.csv")
+#write.csv(broom_three_way_model, file = "~/optimizing-memory/results/three_way_model_neg_intercept.csv")
 
 ggplot(broom_three_way_model, aes(x=reorder(term, estimate), y=estimate)) +
   geom_errorbar(aes(ymin=conf.low, ymax=conf.high), 
@@ -53,14 +59,14 @@ pwpp(trial_type.emm)
 emmeans(RG.max_AUC, ~ trial_type)
 
 
-## fitting linear instead of categorical model,  no correction for multiple comparisons needed.
+## fitting linear instead of categorical model, no correction for multiple comparisons needed.
 
 sub_recalled_score_df$trial_type_num <- sub_recalled_score_df$trial_type %>% as.numeric() - 2
 linear_model <- lmer(sub_recalled ~ trial_type_num * AUC_cent + (1 | subject), data=sub_recalled_score_df)
 summary(linear_model)
 
 broom_linear_model <- tidy(linear_model, conf.int=TRUE)
-write.csv(broom_linear_model, file = "~/optimizing-memory/results/linear_model.csv")
+# write.csv(broom_linear_model, file = "~/optimizing-memory/results/linear_model.csv")
 
 max_AUC <- max(sub_recalled_score_df$AUC_cent)
 RG.max_AUC <- ref_grid(linear_model, at = list(AUC_cent = max_AUC), cov.keep = "trial_type_num" )
@@ -70,4 +76,4 @@ plot(trial_type_num.emm, comparisons = TRUE)
 pwpp(trial_type_num.emm)
 
 broom_max_auc_emmeans <- tidy(trial_type_num.emm, conf.int=TRUE)
-write.csv(broom_max_auc_emmeans, file = "~/optimizing-memory/results/max_auc_emmeans_linear.csv")
+# write.csv(broom_max_auc_emmeans, file = "~/optimizing-memory/results/max_auc_emmeans_linear.csv")
