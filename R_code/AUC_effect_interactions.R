cbcc16f24b65056fe5064f4879d37b93e1041431
Joseph Rudoler <jrudoler56@gmail.com>
2024-06-16 19:20:48 -0400

    update R code

--- R_code/AUC_effect_interactions.R
+++ R_code/AUC_effect_interactions.R
@@ -20,29 +20,29 @@ sub_recalled_score_df <- data |> group_by(subject, trial_type, AUC) |>
     AUC_cent = AUC - .5 # center AUC at .5 (chance level prediction) for easier interpretation
   )
 # model with fixed effects of trial type and AUC and interaction, also random intercepts for subject
-three_way_model <- lmer(sub_recalled ~ trial_type * AUC_cent + (1 | subject), data=sub_recalled_score_df)
-summary(three_way_model)
-
-broom_three_way_model <- tidy(three_way_model, conf.int=TRUE)
-#write.csv(broom_three_way_model, file = "~/optimizing-memory/results/three_way_model_neg_intercept.csv")
-
-ggplot(broom_three_way_model, aes(x=reorder(term, estimate), y=estimate)) +
-  geom_errorbar(aes(ymin=conf.low, ymax=conf.high), 
-                width = 0.2,size  = 1,
-                position = "dodge", color="turquoise4") +
-  geom_hline(yintercept = 0, color = "red", size = 1) +
-  geom_point() + coord_flip()
+# three_way_model <- lmer(sub_recalled ~ trial_type * AUC_cent + (1 | subject), data=sub_recalled_score_df)
+# summary(three_way_model)
+# 
+# broom_three_way_model <- tidy(three_way_model, conf.int=TRUE)
+# #write.csv(broom_three_way_model, file = "~/optimizing-memory/results/three_way_model_neg_intercept.csv")
+# 
+# ggplot(broom_three_way_model, aes(x=reorder(term, estimate), y=estimate)) +
+#   geom_errorbar(aes(ymin=conf.low, ymax=conf.high), 
+#                 width = 0.2,size  = 1,
+#                 position = "dodge", color="turquoise4") +
+#   geom_hline(yintercept = 0, color = "red", size = 1) +
+#   geom_point() + coord_flip()
 
 
 #ANOVA for interaction terms
-anova(three_way_model)
+# anova(three_way_model)
 
-#post-hoc test of pairwise differences in slopes
-auc.emt <- emtrends(three_way_model, pairwise ~ trial_type, var = "AUC_cent")
-#no p-value adjustment
-summary(auc.emt, adjust = "none")
-#tukey adjustment (probably correct)
-summary(auc.emt)
+# #post-hoc test of pairwise differences in slopes
+# auc.emt <- emtrends(three_way_model, pairwise ~ trial_type, var = "AUC_cent")
+# #no p-value adjustment
+# summary(auc.emt, adjust = "none")
+# #tukey adjustment (probably correct)
+# summary(auc.emt)
 
 #post-hoc test of pairwise differences in trial-type at highest AUC
 max_AUC <- max(sub_recalled_score_df$AUC_cent)
@@ -68,12 +68,12 @@ summary(linear_model)
 broom_linear_model <- tidy(linear_model, conf.int=TRUE)
 # write.csv(broom_linear_model, file = "~/optimizing-memory/results/linear_model.csv")
 
-# max_AUC <- max(sub_recalled_score_df$AUC_cent)
-# RG.max_AUC <- ref_grid(linear_model, at = list(AUC_cent = max_AUC), cov.keep = "trial_type_num" )
-# trial_type_num.emm <- emmeans(RG.max_AUC, specs=c("trial_type_num"))
-# plot(trial_type_num.emm)
-# plot(trial_type_num.emm, comparisons = TRUE)
-# pwpp(trial_type_num.emm)
-# 
-# broom_max_auc_emmeans <- tidy(trial_type_num.emm, conf.int=TRUE)
-# write.csv(broom_max_auc_emmeans, file = "~/optimizing-memory/results/max_auc_emmeans_linear.csv")
+max_AUC <- max(sub_recalled_score_df$AUC_cent)
+RG.max_AUC <- ref_grid(linear_model, at = list(AUC_cent = max_AUC), cov.keep = "trial_type_num" )
+trial_type_num.emm <- emmeans(RG.max_AUC, specs=c("trial_type_num"))
+plot(trial_type_num.emm)
+plot(trial_type_num.emm, comparisons = TRUE)
+pwpp(trial_type_num.emm)
+
+broom_max_auc_emmeans <- tidy(trial_type_num.emm, conf.int=TRUE)
+write.csv(broom_max_auc_emmeans, file = "~/optimizing-memory/results/max_auc_emmeans_linear.csv")
